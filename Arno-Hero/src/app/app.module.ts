aac290dacac8c8ca38af718717a3f261bd2304e3
WhiteThundra <87227088+WhiteThundra@users.noreply.github.com>
2022-03-05 13:14:14 +0200

    4. Add services

--- Arno-Hero/src/app/app.module.ts
+++ Arno-Hero/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { MessageComponent } from './components/message/message.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessageComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
