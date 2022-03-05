ed2438f443523153c820c38d742ffdd15261feca
WhiteThundra <87227088+WhiteThundra@users.noreply.github.com>
2022-03-05 13:14:14 +0200

    4. Add services

--- Arno-Hero/src/app/components/heroes/heroes.component.ts
+++ Arno-Hero/src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
+import { MessageService } from './../../services/message.service';
 import { Hero } from '../../hero';
 import { Component, OnInit } from '@angular/core';
 import { HEROES } from '../../mock-heroes';
+import { HeroService } from 'src/app/services/hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +10,21 @@ import { HEROES } from '../../mock-heroes';
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = HEROES;
   selectedHero?: Hero;
+  heroes: Hero[] = [];
 
-  constructor() {}
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add('HeroesComponent: Selected hero id=${hero.id}');
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
