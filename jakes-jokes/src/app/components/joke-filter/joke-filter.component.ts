7d067313e5ac2d20d8784ec0c9b56017c539186b
jakesfcoetz <jakesfcoetz@gmail.com>
2022-03-06 16:56:20 +0200

    add basic api call

--- jakes-jokes/src/app/components/joke-filter/joke-filter.component.ts
+++ jakes-jokes/src/app/components/joke-filter/joke-filter.component.ts
@@ -1,15 +1,32 @@
+/**
+ * @file          joke-filter.component
+ * @description   Contains all elements pertaining to the joke filter
+ */
+
 import { Component, OnInit } from '@angular/core';
+//--- Service
+import { JokeService } from 'src/app/services/joke.service';
 
 @Component({
   selector: 'app-joke-filter',
   templateUrl: './joke-filter.component.html',
-  styleUrls: ['./joke-filter.component.scss']
+  styleUrls: ['./joke-filter.component.scss'],
 })
 export class JokeFilterComponent implements OnInit {
+  /**----------------------------------------------------------------
+   * @name  Constructor
+   */
+  constructor(private jokeService: JokeService) {}
 
-  constructor() { }
-
+  /**----------------------------------------------------------------
+   * @name          ngOnInit
+   * @description   Update various values on start-up
+   */
   ngOnInit(): void {
+    this.jokeService.getJoke().subscribe((joke) => {
+      if (!joke.error) {
+        console.log('RES:', joke);
+      }
+    });
   }
-
 }
