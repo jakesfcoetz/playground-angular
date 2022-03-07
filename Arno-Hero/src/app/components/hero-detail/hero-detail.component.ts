105a32fe82b2fa25c387fc5b083909dd731efd6c
WhiteThundra <87227088+WhiteThundra@users.noreply.github.com>
2022-03-07 19:49:51 +0200

    back werk nie + routing van detail werk nie

--- Arno-Hero/src/app/components/hero-detail/hero-detail.component.ts
+++ Arno-Hero/src/app/components/hero-detail/hero-detail.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from 'src/app/hero';
-
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from 'src/app/services/hero.service';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.scss'],
 })
 export class HeroDetailComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-
+  constructor(private heroService: HeroService, private route: ActivatedRoute, private location: Location) {}
   @Input() hero?: Hero;
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+  // get hero werk nie reg nie
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+
+  // die back wil nie werk nie
+  //-goBack(): void {
+  // this.location.back;
+  //}
 }
