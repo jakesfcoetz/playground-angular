b6d9ac27c9bb575ce7d157ce54ef32860e7face0
jakesfcoetz <jakesfcoetz@gmail.com>
2022-03-08 16:04:52 +0200

    final touches

--- jakes-jokes/src/app/services/joke.service.ts
+++ jakes-jokes/src/app/services/joke.service.ts
@@ -18,18 +18,9 @@ export class JokeService {
    * @param       {JokeRequestParamsI} params? (optional) either url or query parameters
    * @returns     {JokeResponseI} Observable
    */
-  getJoke(params?: JokeRequestParamsI): Observable<JokeResponseI> {
-    console.log('params:', params);
-
-    let endpoint: string = jokeApiC.url;
-    //--- Add URL params (/)
-    if (Array.isArray(params?.category) && params?.category.length) {
-      endpoint += '/' + params?.category.join(',');
-    } else {
-      endpoint += '/any';
-    }
+  getJoke(params: JokeRequestParamsI): Observable<JokeResponseI> {
     return this.http
-      .get<JokeResponseI>(endpoint, params?.contains ? { params: { contains: params.contains } } : {})
+      .get<JokeResponseI>(jokeApiC.url + `/${params.category}`, params?.contains ? { params: { contains: params.contains } } : {})
       .pipe(catchError((err) => of(err)));
   }
 }
