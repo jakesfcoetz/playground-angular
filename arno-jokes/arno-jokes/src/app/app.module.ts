b11e5464afacc599ff125b216fe5373afc75e108
WhiteThundra <87227088+WhiteThundra@users.noreply.github.com>
2022-03-13 13:40:17 +0200

    navbar comp + joke comp

--- arno-jokes/arno-jokes/src/app/app.module.ts
+++ arno-jokes/arno-jokes/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JokesComponent } from './components/jokes/jokes.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
