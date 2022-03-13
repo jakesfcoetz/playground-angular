bf1979f1f3cb0eea342644ad95d117bee2b6a127
WhiteThundra <87227088+WhiteThundra@users.noreply.github.com>
2022-03-13 13:40:17 +0200

    navbar comp + joke comp

--- arno-jokes/arno-jokes/src/app/app-routing.module.ts
+++ arno-jokes/arno-jokes/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AppComponent } from './app.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JokesComponent } from './components/jokes/jokes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'jokes', component: JokesComponent},
+{path:'menu', component: NavbarComponent},
+{path:'', component:NavbarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
