292ecc38f0eefb5e2c900717b73c6e8a2178f6e1
WhiteThundra <87227088+WhiteThundra@users.noreply.github.com>
2022-03-15 21:51:40 +0200

    change page look + http call fail

--- arno-jokes/arno-jokes/src/app/app-routing.module.ts
+++ arno-jokes/arno-jokes/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { AppComponent } from './app.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { JokesComponent } from './components/jokes/jokes.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 const routes: Routes = [
 {path:'jokes', component: JokesComponent},
-{path:'menu', component: NavbarComponent},
-{path:'', component:NavbarComponent ,pathMatch:"full"},
+{path:'menu', component: WelcomeComponent},
+{path:'', component: WelcomeComponent}
+
 ];
 
 @NgModule({
